001c29047e7497f0307e4eb5129ea696bee62f82
jshabree <jshabree@emich.edu>
2021-02-09 02:10:01 -0500

    querying with listing objects

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,13 @@
 import express from "express";
-import {ApolloServer} from "apollo-server-express";
-import {schema} from "./graphql";
+import { ApolloServer } from "apollo-server-express";
+import { schema } from "./graphql";
 
 // assigning express instance to a variable app
 const app = express();
 const port = 9000;
 
 const server = new ApolloServer({ schema });
-server.applyMiddleware({app, path: '/api'});
-
-
+server.applyMiddleware({ app, path: "/api" });
 
 app.listen(port);
 
