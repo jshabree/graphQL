1c0cc47aab223e6dd6eb9207246ece952f8a6ef4
jshabree <jshabree@emich.edu>
2021-02-09 01:40:11 -0500

    setting up graphql with apollo server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,31 +1,15 @@
 import express from "express";
-import bodyParser from "body-parser";
-import { listings } from "./listings";
+import {ApolloServer} from "apollo-server-express";
+import {schema} from "./graphql";
 
 // assigning express instance to a variable app
 const app = express();
 const port = 9000;
 
-app.use(bodyParser.json());
+const server = new ApolloServer({ schema });
+server.applyMiddleware({app, path: '/api'});
 
-// mock GET call
-app.get("/listings", (_req, res) => {
-  return res.send(listings);
-});
 
-// mock POST request to delete routes
-app.post("/delete-listing", (req, res) => {
-  const id: string = req.body.id;
-
-  // find the listing we want to delete
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.send(listings.splice(i, 1));
-    }
-  }
-
-  return res.send('failed to delete listing');
-});
 
 app.listen(port);
 
